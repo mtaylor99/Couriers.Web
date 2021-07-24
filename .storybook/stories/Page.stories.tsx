2563ab9cbe82ace567a140ec9d0f867cd630a66d
Mathew Taylor <mtaylor99@sky.com>
2021-07-24 11:27:15 +0100

    Storybook added

---
+++ .storybook/stories/Page.stories.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-extraneous-dependencies */
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Page } from "./Page";
+import * as HeaderStories from "./Header.stories";
+
+export default {
+  title: "Example/Page",
+  component: Page,
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  ...HeaderStories.LoggedIn.args,
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
